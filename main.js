43c7ee632e72e296b64c4baf65a65c15c8c2a564
Aniket Kudale <aniket.kudale@hotmail.com>
2023-04-30 01:40:00 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const {
+    app,
+    Tray,
+    Menu,
+    shell,
+    globalShortcut,
+    clipboard
+} = require('electron');
+const options = require('./options.json');
+const path = require('path');
+
+app.on('ready', function() {
+    const icon = path.join(__dirname + '/icon.png');
+
+    const tray = new Tray(icon);
+
+    const template = [];
+
+    for (let i = 0; i < options.length; i++) {
+        if (i < 9) {
+            globalShortcut.register('CommandOrControl+' + Number(i + 1), () => {
+                clipboard.writeText(options[i].text)
+            })
+            template.push({
+                label: i + 1 + ": " + options[i].label,
+                click: function() {
+                    clipboard.writeText(options[i].text)
+                }
+            })
+        } else {
+            globalShortcut.register('CommandOrControl+' + 0, () => {
+                clipboard.writeText(options[i].text)
+            })
+            template.push({
+                label: 0 + ": " + options[i].label,
+                click: function() {
+                    clipboard.writeText(options[i].text)
+                }
+            })
+        }
+    }
+
+    template.push({
+        label: "Made with ❤️",
+        click: function() {
+            shell.openExternal('https://aniket.co');
+        }
+    })
+
+    const ctxMenu = Menu.buildFromTemplate(template);
+
+    tray.setToolTip('Commands Helper');
+    tray.setContextMenu(ctxMenu);
+});
+
+app.commandLine.appendSwitch("disable-software-rasterizer");
+app.commandLine.appendSwitch('disable-gpu');
